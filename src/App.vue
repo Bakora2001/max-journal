90e19fa75ceb55e2354c5263cca5ddd271cb92d3
Bakora2001 <maxwellbakora@gmail.com>
2023-11-27 18:15:34 +0300

    Staged the changes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
-  <div id="app">
-    <div>
-      <mainDiary/>
+  <div class="md:flex sm:fle-col" id="app">
+    <div class="sm:full h-auto md:w-[500px] md:h-[600px]">
+      <sideBar @entry-saved="navigateToEntries"/>
+    </div>
+    <div class="w-full px-4">
+      <mainDiary @entry-saved="navigateToEntries"/>
     </div>
   </div>
 </template>
 
 <script>
-
+import sideBar from './components/sideBar.vue';
 import mainDiary from './components/mainDiary.vue'; //I have imported this from mainDIary.vue
 
 export default {
   name: 'App',
   components: {
-    mainDiary
+    mainDiary,
+    sideBar
   },
+  methods: {
+    navigateToEntries() {
+      this.$router.push('/mainDiaryVue');
+    }
+  }
 };
 </script>
 
