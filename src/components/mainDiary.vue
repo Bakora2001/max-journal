c37fabd8eb9bea89e10a8fb04f5a3cd11ab6cec2
Bakora2001 <maxwellbakora@gmail.com>
2023-11-28 17:52:25 +0300

    Introduced json server to my project

--- src/components/mainDiary.vue
+++ src/components/mainDiary.vue
@@ -1,56 +1,87 @@
-
+<!-- mainDiary.vue -->
 <template>
-    <div>
-      <div class="flex flex-col gap-4 mt-2 mb-6">
-        <input v-model="newEntry.heading" placeholder="Enter heading" class="p-2 mb-2" />
-        <input v-model="newEntry.content" placeholder="Enter content" class="p-2">
-        <button @click="saveEntry" class="bg-blue-500 text-white p-2">Save</button>
-      </div>
-  
-      <!-- Rendering the diary entry from the vue component I created (diaryEntry.vue) -->
-      <diary-entry
-        v-for="entry in entries"
-        :key="entry.id"
-        :entry="entry"
-        @edit="editEntry"
-        @delete="deleteEntry"
-      ></diary-entry>
+  <div>
+    <div class="flex flex-col gap-4 mt-2 mb-6">
+      <input v-model="newEntry.heading" placeholder="Enter heading" class="p-2 mb-2" />
+      <input v-model="newEntry.content" placeholder="Enter content" class="p-2">
+      <button @click="saveEntry" class="bg-blue-500 text-white p-2">Save</button>
     </div>
-  </template>
-  
-  <script>
-  import diaryEntry from '../components/diaryEntry.vue';
-  
-  export default {
-    components: {
-      diaryEntry
+
+    <!-- Rendering the diary entry from the vue component I created (diaryEntry.vue) -->
+    <diary-entry
+      v-for="entry in entries"
+      :key="entry.id"
+      :entry="entry"
+      @edit="editEntry"
+      @delete="deleteEntry"
+    ></diary-entry>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  components: {
+    // ... existing component imports
+  },
+  data() {
+    return {
+      entries: [],
+      newEntry: {
+        heading: "",
+        content: "",
+      },
+    };
+  },
+  created() {
+    this.fetchEntries();
+  },
+  methods: {
+    async fetchEntries() {
+      try {
+        const response = await axios.get("http://localhost:3000/entries");
+        this.entries = response.data;
+      } catch (error) {
+        console.error("Error fetching entries:", error);
+      }
     },
-    data() {
-      return {
-        entries: [],
-        newEntry: {
-          heading: '',
-          content: '',
-        },
-      };
-    },
-    methods: {
-      saveEntry() {
-        this.entries.push({
+    async saveEntry() {
+      try {
+        const response = await axios.post("http://localhost:3000/entries", {
           id: Date.now(),
           heading: this.newEntry.heading,
           content: this.newEntry.content,
         });
-        this.newEntry.heading = '';
-        this.newEntry.content = '';
-      },
-      editEntry(entryToEdit) {
-        const index = this.entries.findIndex(entry => entry.id === entryToEdit.id);
-        this.entries[index] = { ...entryToEdit };
-      },
-      deleteEntry(entryToDelete) {
-        this.entries = this.entries.filter(entry => entry.id !== entryToDelete.id);
-      },
+        this.entries = response.data;
+        this.newEntry.heading = "";
+        this.newEntry.content = "";
+        // After saving, it fetch the new list of entries after saving a new one
+        this.fetchEntries();
+      } catch (error) {
+        console.error("Error saving entry:", error);
+      }
     },
-  };
-  </script>
+    async editEntry(entryToEdit) {
+      try {
+        const response = await axios.put(
+          `http://localhost:3000/entries/${entryToEdit.id}`,
+          entryToEdit
+        );
+        const index = this.entries.findIndex((entry) => entry.id === response.data.id);
+        this.$set(this.entries, index, response.data);
+      } catch (error) {
+        console.error("Error editing entry:", error);
+      }
+    },
+    async deleteEntry(entryToDelete) {
+      try {
+        await axios.delete(`http://localhost:3000/entries/${entryToDelete.id}`);
+        this.entries = this.entries.filter((entry) => entry.id !== entryToDelete.id);
+      } catch (error) {
+        console.error("deleting entry error:", error);
+      }
+    },
+  },
+};
+</script>
